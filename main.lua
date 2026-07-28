6eaad1a0fda6be1bc121eeb1cbfcdf55337b72c1
cantfindme <106627718+catfueledmayhem@users.noreply.github.com>
2026-07-29 01:51:26 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -102,17 +102,14 @@ function library:CreateNotification(title, body, typeOf, bodyimage)
 		Images:FindFirstChild(typeOf).Visible = true
 		spawn(function()
             	   local frame = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):FindFirstChild("NekoLolis").Notification
-            	   frame:TweenPosition(UDim2.new(0.774, 0,0.778, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Sine, 1.5)
+            	   frame:TweenPosition(UDim2.new(0.774, 0,0.11, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Sine, 1.5)
             	   wait(5)
-					if game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):FindFirstChild("NekoLolis") then
-	            	   frame:TweenPosition(UDim2.new(1.1, 0,0.778, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Sine, 1.5)
-	            	   wait(1.6)
-	            	   game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):FindFirstChild("NekoLolis"):Destroy()
-					end
+            	   frame:TweenPosition(UDim2.new(1.1, 0,0.11, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Sine, 1.5)
+            	   wait(1.6)
+            	   game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):FindFirstChild("NekoLolis"):Destroy()
 		end)
 	end
 	
 	return library
 end
-
 return library
